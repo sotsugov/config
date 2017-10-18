2ec8e542d2ee7204bb8560aec52f554186f6f59f
Igor Sotsugov <reveous@gmail.com>
2017-10-18 09:30:33 +0100

    Disable hostname setting

--- osx.sh
+++ osx.sh
@@ -13,10 +13,10 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 ###############################################################################
 
 # Set computer name (as done via System Preferences → Sharing)
-sudo scutil --set ComputerName "foundation"
-sudo scutil --set HostName "foundation"
-sudo scutil --set LocalHostName "foundation"
-sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "foundation"
+# sudo scutil --set ComputerName "foundation"
+# sudo scutil --set HostName "foundation"
+# sudo scutil --set LocalHostName "foundation"
+# sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "foundation"
 
 # Set standby delay to 24 hours (default is 1 hour)
 sudo pmset -a standbydelay 86400
